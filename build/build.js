385bdc913ba747cc1a0b5e0d87fee88e180ad050
Konstantin Kuzmin <kkuzmin@alertlogic.com>
2022-04-29 11:12:41 -0400

    Move to Node14. Add missing regions into readme

--- build/build.js
+++ build/build.js
@@ -37,7 +37,23 @@ var source = {
     ]
 };
 
-var awsRegions      = ['us-east-1', 'us-east-2', 'us-west-1','us-west-2', 'eu-west-1', 'ap-northeast-1', 'ap-southeast-2', 'ap-southeast-1', 'eu-central-1'];
+var awsRegions = [
+    'ap-northeast-1',
+    'ap-northeast-2',
+    'ap-south-1',
+    'ap-southeast-1',
+    'ap-southeast-2',
+    'ca-central-1',
+    'eu-central-1',
+    'eu-west-1',
+    'eu-west-2',
+    'eu-west-3',
+    'sa-east-1',
+    'us-east-1',
+    'us-east-2',
+    'us-west-1',
+    'us-west-2'
+];
 
 /*
  * Create the node_modules directory so that it exists for installation regardless of module definitions for deployment
